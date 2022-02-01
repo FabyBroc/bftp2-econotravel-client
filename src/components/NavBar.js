5c10e03d00703026694010198d4ecfa7948d50ff
RosaPs <rosapacheco.contact@gmail.com>
2022-02-01 15:33:23 +0100

    añadido logo y medidas, color del navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ function NavBar(){
 
     //2. Dentro del retorno introducimos el html
     return(
+
         <div className="NavBar">
             <h1>Este es el navBar</h1>
-            
         </div>
     );
 }
