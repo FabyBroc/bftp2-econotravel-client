42cb1ee5a3c31e19e98ac1b7990e8b638f2ff560
FabyBroc <fabybroc@hotmail.com>
2022-02-09 15:10:05 +0100

    arreglado card.css, primera parte

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,17 @@ function Card({experience}) {
 
     return (
 
-        <div className="card">
-            <img src={experience.imgUrl}/>
-            <h1>{experience.name}</h1>
-            <p>Descripción: {experience.descripcion}</p>
-            <span>Duración: {experience.time}</span>
-            <span>Precio: {experience.price}</span>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
-
+            <div className="card">
+                <img className="image-card" src={experience.imgUrl}/>
+                <h1 className="card-title">{experience.name}</h1>
+                <p className="card-text">Descripción: {experience.descripcion}</p>
+                <span className="card-title"><b>Duración:</b> {experience.time} h.</span>
+                <span className="card-title"><b>Precio:</b> {experience.price} €</span>
+                <div className="botones">
+                <button className="btn">Edit</button>
+                <button className="btn">Delete</button>
+                </div>
+            </div>
 
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './NavBar.css'
-import { Nav } from "rsuite";
+
 
 
 function NavBar () {
 
     return (
-        <Nav className="nav">
-            <Nav.Item className="home" href="/" >Home</Nav.Item>
-            <Nav.Item className="add" href="#">Añadir</Nav.Item>
-            <Nav.Item className="search" href="#">Buscar</Nav.Item>
-        </Nav>
+        <div className="nav">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
         )
 }
 
