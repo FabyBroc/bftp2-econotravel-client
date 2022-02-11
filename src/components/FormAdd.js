f73e4bf87b59edffa71b64f2b7201e92a41023d8
GraciaLazaro <gracia2092@gmail.com>
2022-02-11 12:30:50 +0100

    ECON-2 | Añadiendo formulario

---
+++ src/components/FormAdd.js
@@ -0,0 +1,107 @@
+import React, { Fragment, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const Formulario = () => {
+
+    const {register, errors, handleSubmit} = useForm();
+
+    const [entradas, setentradas] = useState([]);
+
+    const procesarFormulario = (data, e) => {
+        console.log(data);
+        setentradas([
+            ...entradas,
+            data
+        ])
+        // limpiar campos
+        e.target.reset();
+    }
+
+    return (
+        <Fragment>
+            <h1>FORMULARIO</h1>
+            <form onSubmit={handleSubmit(procesarFormulario)}>
+                <input
+                    name="titulo"
+                    ref={
+                        register({
+                            required: {value:true, message: 'Ingrese un nombre'}
+                        })
+                    }
+                    className="form-control my-2"
+                    placeholder="Ingrese título"
+                ></input>
+                <span className="text-danger text-small d-block mb-2">
+                    {errors?.titulo?.message}
+                </span>
+
+
+
+                <input
+                    name="imagen"
+                    ref={
+                        register({
+                            required: {value:true, message: 'Ingrese Url Imagen'}
+                        })
+                    }
+                    className="form-control my-2"
+                    placeholder="Ingrese Url Imagen"
+                ></input>
+                <span className="text-danger text-small d-block mb-2">
+                    {errors?.descripcion?.message}
+                </span>
+
+
+
+
+                <input
+                    name="time"
+                    ref={
+                        register({
+                            required: {value:true, message: 'Ingrese duración'}
+                        })
+                    }
+                    className="form-control my-2"
+                    placeholder="Ingrese duración"
+                ></input>
+                <span className="text-danger text-small d-block mb-2">
+                    {errors?.descripcion?.message}
+                </span>
+
+
+
+
+                <input
+                    name="descripcion"
+                    ref={
+                        register({
+                            required: {value:true, message: 'Ingrese descripción'}
+                        })
+                    }
+                    className="form-control my-2"
+                    placeholder="Ingrese descripción"
+                ></input>
+                <span className="text-danger text-small d-block mb-2">
+                    {errors?.descripcion?.message}
+                </span>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                >
+                    Agregar
+                </button>
+            </form>
+            <ul className="mt-2">
+                {
+                    entradas.map((item, index) =>
+                        <li key={index}>
+                            {item.titulo} - {item.descripcion}
+                        </li>
+                    )
+                }
+            </ul>
+        </Fragment>
+    );
+}
+
+export default Formulario;
